78a062ccc7053aa8c3e23c63f3b65c24d6edaf1f
Anthony Bosek <anthony.bosek@gmail.com>
2023-10-23 19:04:30 -0700

    all done

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -1,8 +1,5 @@
-from versions import (
-    python_version,
-    requests_version,
-    pytest_version
-)
+from versions import python_version, requests_version, pytest_version
+
 
 def test_python_version():
     version_info = python_version()
